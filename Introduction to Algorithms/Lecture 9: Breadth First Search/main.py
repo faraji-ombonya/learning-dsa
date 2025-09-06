4ce8f0e99698971522f4adb26208a70417a1897c
Faraji Ombonya <farajiombonya@gmail.com>
2025-09-06 15:04:05 +0300

    BFS

--- Introduction to Algorithms/Lecture 9: Breadth First Search/main.py
+++ Introduction to Algorithms/Lecture 9: Breadth First Search/main.py
@@ -24,3 +24,53 @@ G3 = {
     "f": ["d", "e"],
     "g": [],
 }
+
+
+G4 = {
+    "s": ["r", "v", "u"],
+    "r": ["w", "s", "t"],
+    "u": ["t", "s", "y"],
+    "t": ["u", "s", "r"],
+    "y": ["u", "v", "x"],
+    "v": ["w", "s", "y"],
+    "w": ["z", "x", "v", "r"],
+    "x": ["z", "w", "y"],
+    "z": ["x", "w"],
+}
+
+
+def bfs():
+    stack = []
+
+    print("new stack::", stack)
+
+    stack.append("s")
+
+    print(" stack after adding the source node::", stack)
+
+    i = 0
+
+    while stack:
+
+        # Visit node
+        node = stack.pop()
+        print("POPPED NODE::", node)
+
+        print("STACK After POP::", stack)
+
+        print("NEIGHBOURS OF THE SOURCE NODE::", G4[node])
+
+        # Discover neighbours
+        for neigbour in G4[node]:
+            stack.append(neigbour)
+
+        print(f"STACk on Iter::: {i}", stack)
+
+        i += 1
+
+        if i == 1:
+            return
+
+
+if __name__ == "__main__":
+    bfs()
